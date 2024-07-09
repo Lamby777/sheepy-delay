77c3ec515bdda39f957a042c9900ea5f26771f79
Cherry <cherry@sparklet.org>
2024-07-08 22:24:38 -0400

    gain -> wet (linear scale)

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,6 @@ use std::sync::Arc;
 mod ringbuffer;
 use ringbuffer::RingBuffer;
 
-// This is a shortened version of the gain example with most comments removed, check out
-// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
-// started
-
 struct SheepyDelay {
     params: Arc<SheepyDelayParams>,
     ring_buffer: RingBuffer<f32>,
@@ -15,12 +11,8 @@ struct SheepyDelay {
 
 #[derive(Params)]
 struct SheepyDelayParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
-    #[id = "gain"]
-    pub gain: FloatParam,
+    #[id = "wet"]
+    pub wet: FloatParam,
 }
 
 impl Default for SheepyDelay {
@@ -35,29 +27,13 @@ impl Default for SheepyDelay {
 impl Default for SheepyDelayParams {
     fn default() -> Self {
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
-            gain: FloatParam::new(
-                "Gain",
-                util::db_to_gain(0.0),
-                FloatRange::Skewed {
-                    min: util::db_to_gain(-30.0),
-                    max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
-                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
-                },
-            )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
-            .with_smoother(SmoothingStyle::Logarithmic(50.0))
-            .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
-            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+            wet: FloatParam::new("Wet", 0.0, FloatRange::Linear { min: 0.0, max: 1.0 })
+                .with_unit("%")
+                // There are many predefined formatters we can use here. If the gain was stored as
+                // decibels instead of as a linear gain value, we could have also used the
+                // `.with_step_size(0.1)` function to get internal rounding.
+                .with_value_to_string(formatters::v2s_f32_percentage(2))
+                .with_string_to_value(formatters::s2v_f32_percentage()),
         }
     }
 }
@@ -129,10 +105,10 @@ impl Plugin for SheepyDelay {
     ) -> ProcessStatus {
         for channel_samples in buffer.iter_samples() {
             // Smoothing is optionally built into the parameters themselves
-            let gain = self.params.gain.smoothed.next();
+            let wet = self.params.wet.smoothed.next();
 
             for sample in channel_samples {
-                *sample *= gain;
+                *sample *= wet;
             }
         }
 
